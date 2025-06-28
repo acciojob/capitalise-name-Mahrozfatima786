21049df7a7250bbe9b3fbbcd0b58e9a02d459962
Mahrozfatima786 <mahrozfatima156@gmail.com>
2025-06-28 11:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+    const input = document.getElementById("fname");
+
+      input.addEventListener("input", function () {
+        // Change actual value to uppercase
+        this.value = this.value.toUpperCase();
+      });
